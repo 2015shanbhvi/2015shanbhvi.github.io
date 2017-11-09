86dbc63d30be1904e64034e633593c5129e4d43a
shanbha2 <shanbha2@illinois.edu>
2017-11-09 08:40:26 -0600

    found winning positions

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,6 +1,20 @@
 
+var  winning_positions_array= new Array(); //array to hold the winning indices 
 
+    winning_positions_array.push([1, 2, 3]);//not sure how else other than hardcode it
+    winning_positions_array.push([1, 4, 7]);//start top left 
+    winning_positions_array.push([1, 5, 9]);
+    winning_positions_array.push([2, 5, 8]);
+    winning_positions_array.push([3, 5, 7]);
+    winning_positions_array.push([3, 6, 9]);
+    winning_positions_array.push([4, 5, 6]);
+    winning_positions_array.push([7, 8, 9]);
 
+function draw_Board() {
+    var theGrid = document.getElementById("tic");
+    var counter = 1;
+
+}
 
 
 
